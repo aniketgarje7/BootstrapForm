f5f609ac8f414f54c5b85f907182cde389d5c68a
aniketgarje7 <garjeaniket7@gmail.com>
2022-11-16 23:28:54 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React  from "react"
-import Child from './Component/Child'
 
+import FormExample from "./Component/form";
 import 'bootstrap/dist/css/bootstrap.min.css';
-class App extends React.Component{
-  constructor(props){
-    super(props)
-      this.state = {
-        count:0,
-        child:true
-      }
+
+ 
 
     
+  const App = ()=>{
+    return <div>aniket
+      <FormExample/>
+    </div>
+        
   }
-  render(){
-    return <>
-       <h1>APP</h1>
-       <h1>{this.state.count}</h1>
-       <button onClick={()=>{this.setState({count:8})}}>increament</button>
-       {this.state.child && <Child/>}
-        <button onClick={()=>{this.setState({child:!this.state.child})}}>willUnmoun</button>
-       </>
-    }
-}
+      
+    
+
+
 
 export default App
